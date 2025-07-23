eae89b15394b1805b10fa1c04bb5ff52131b3e38
siieegee <115536658+siieegee@users.noreply.github.com>
2025-07-23 10:01:20 +0800

    arranged the routes in web.php

--- routes/web.php
+++ routes/web.php
@@ -4,32 +4,37 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
+// Public Routes
 Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
-Route::get('/dashboard', [PostController::class, 'index'])
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+Route::view('/about', 'about')->name('about');
 
-Route::middleware('auth')->group(function () {
-    Route::resource('posts', PostController::class);
-});
+// Protected routes
+Route::middleware(['auth', 'verified'])->group(function () {
+    // Dashboard (list posts)
+    Route::get('/dashboard', [PostController::class, 'index'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+    // Custom route for delete confirmation before actual destroy
+    Route::get('/posts/{post}/delete', [PostController::class, 'confirmDelete'])->name('posts.delete');
+
+    // Resource routes for posts (includes index, create, store, show, edit, update, destroy)
+    Route::resource('posts', PostController::class)->except(['index']); 
+    // index route excluded because it's handled by dashboard above
+
+    // Profile routes
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+require __DIR__.'/auth.php';
+
+/*
 Route::get('/posts/{post}', [PostController::class, 'show'])->name('posts.show');
 Route::get('/posts/create', [PostController::class, 'create'])->name('posts.create');
 Route::get('/posts/{post}/edit', [PostController::class, 'edit'])->name('posts.edit');
 Route::delete('/posts/{post}', [PostController::class, 'destroy'])->name('posts.destroy');
-;
 Route::post('/posts', [PostController::class, 'store'])->name('posts.store');
-
-Route::view('/about', 'about')->name('about');
-Route::get('/posts/{post}/delete', [PostController::class, 'confirmDelete'])->name('posts.delete');
-
-require __DIR__.'/auth.php';
+*/

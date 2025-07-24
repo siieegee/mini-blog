2c5e22e0418749cb16968fb57294b228f557a45c
siieegee <115536658+siieegee@users.noreply.github.com>
2025-07-24 17:21:31 +0800

    removed unnecessary codes

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,20 +5,3 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
-
-document.addEventListener("DOMContentLoaded", function () {
-  document.querySelectorAll(".ripple").forEach((el) => {
-    el.addEventListener("click", function (e) {
-      const circle = document.createElement("span");
-      const rect = el.getBoundingClientRect();
-      circle.classList.add("ripple");
-      circle.style.left = `${e.clientX - rect.left}px`;
-      circle.style.top = `${e.clientY - rect.top}px`;
-      el.appendChild(circle);
-
-      setTimeout(() => {
-        circle.remove();
-      }, 600);
-    });
-  });
-});
